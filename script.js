53970d24c9888714c4f409510195ef6c9fd6d51c
alifferferreirasantos-cmyk <aliffer.ferreira.santos@escola.pr.gov.br>
2026-08-24 08:19:03 -0300

    rght

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector("buton");
+botao.addEventListener("click", botaoClicado);
+
+function botaoClicado(){
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+}
+
